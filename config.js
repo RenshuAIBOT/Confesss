3bd19f23cff3c3306fcc9bd65d96f3862a188af4
RenshuAIBOT <talesrenal230@gmail.com>
2024-11-07 15:58:17 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,17 @@
+const CONFIG = {
+    titleWeb: "Hello:3",
+    introTitle: 'Selamat Datang',
+    introDesc: `Silahkan Klik Lanjut`,
+    btnIntro: 'Lanjut',
+    title: 'Aku Sebenernya Suka Kamu... Mau Ga Jadi Pacarku?',
+    desc: 'Aku Udah Suka Kamu Dari Lama Sebenernya:) ',
+    btnYes: 'Terima',
+    btnNo: 'Tolak',
+    question: 'Eh Di Kenapa Di Terima??!',
+    btnReply: 'Terima',
+    reply: 'Karena Aku Juga Suka Kamu',
+    mess: 'Makasih Udah Mau Terima Aku🥰',
+    messDesc: 'Semoga Hubungan Kita Bertahan Lama:)',
+    btnAccept: 'Terima Kasih',
+    messLink: 'https://renshuaibot.github.io/LopYu/' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
+}
